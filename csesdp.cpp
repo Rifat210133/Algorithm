6ed2a107916e328d7df0e2627f8ddcfcf9d1dda3
Rifat210133 <158846784+Rifat210133@users.noreply.github.com>
2024-02-05 00:21:06 +0600

    Add files via upload
    This a code of dp.....

---
+++ csesdp.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+
+void rifat(){
+    int n;
+    cin >> n;
+    vector<int>dp(n+1);
+    dp[0]=1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=6; j++){
+            if(j<=i){
+                dp[i]=(dp[i]+dp[i-j])%MOD;
+            }
+        }
+    }
+    cout<<dp[n]<<endl;
+}
+
+int main(){
+    int t=1;
+    //cin>>t
+    while(t--){
+        rifat();
+    }
+}
